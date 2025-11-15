8849fa86a7bf16149348bc70d0bb4dcffbc22b72
Krittika Sen <krittikasen6@gmail.com>
2025-11-16 00:36:24 +0530

    Add files via upload

---
+++ mini_alu_arduino1.ino
@@ -0,0 +1,58 @@
+int ledPins[4] = {4, 5, 6, 7}; 
+int carryPin = 8;
+void setup() {
+  Serial.begin(9600);
+  for(int i=0; i<4; i++) pinMode(ledPins[i], OUTPUT);
+  pinMode(carryPin, OUTPUT);
+  Serial.println("4-bit ALU Ready!");
+  Serial.println("Enter: A B opcode");
+  Serial.println("0=ADD, 1=SUB, 2=AND, 3=OR, 4=XOR");
+}
+void loop() {
+  int A,B,opcode;
+  if(Serial.available()) {
+    A = Serial.parseInt();
+    B = Serial.parseInt();
+    opcode = Serial.parseInt();
+    A&=0xF;
+    B&=0xF;
+    int result=0;
+    int carry=0;
+    switch(opcode) {
+      case 0:
+        result = A + B;
+        carry = (result > 15);
+        result &= 0xF;
+        break;
+      case 1:
+        result = A - B;
+        if(result < 0) {
+          carry = 1;
+          result = (result + 16) & 0xF;
+        }
+        break;
+      case 2: 
+        result = A & B;
+        break;
+      case 3: 
+        result = A | B;
+        break;
+      case 4:
+        result = A ^ B;
+        break;
+      default:
+        Serial.println("Invalid opcode!");
+        return;
+    }
+    for(int i=0; i<4; i++)
+      digitalWrite(ledPins[i], (result >> i) & 1);
+    digitalWrite(carryPin, carry);
+    Serial.println("---------------------------");
+    Serial.print("A = "); Serial.println(A);
+    Serial.print("B = "); Serial.println(B);
+    Serial.print("Opcode = "); Serial.println(opcode);
+    Serial.print("Result = "); Serial.println(result);
+    Serial.print("Carry = "); Serial.println(carry);
+    Serial.println("---------------------------");
+  }
+}
